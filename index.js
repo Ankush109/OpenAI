5bd1d0f06e1541ebbefd1e932cc88f9b4a86f4bf
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2022-12-11 00:06:28 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const path = require("path");
 const port = process.env.PORT || 5000;
 const app = express();
 // enable body parser
 app.use(express.json());
 // enable cors
 app.use(express.urlencoded({ extended: false }));
+// enable static files
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/openai", require("./routes/openairoute"));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
